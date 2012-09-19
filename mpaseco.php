ff3f790a2256bc30b9d308ec5da4bc241b576bc8
Lukas Kremsmayr <lukas@esc-clan.net>
2012-09-19 23:25:39 +0200

    updated updatefolder

--- mpaseco.php
+++ mpaseco.php
@@ -57,7 +57,7 @@ define('CONFIG_UTF8ENCODE', false);
 
 // current project version
 
-define('MPASECO_VERSION', '0.42');
+define('MPASECO_VERSION', '0.45');
 
 // A fix for old plugins which checks this constant
 define('XASECO2_VERSION', '5.55');
