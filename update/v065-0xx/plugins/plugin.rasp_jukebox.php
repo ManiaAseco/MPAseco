77cf3a9161d5fa6e43e932a2e26d46cba817814e
Lukas Kremsmayr <lukas@esc-clan.net>
2013-03-28 14:11:33 +0100

    changes from xaseco and added maplistmodified cache empty

--- update/v065-0xx/plugins/plugin.rasp_jukebox.php
+++ update/v065-0xx/plugins/plugin.rasp_jukebox.php
@@ -116,7 +116,10 @@ function rasp_update_maplist($aseco,$array){
                                "\t<filter>" . CRLF .
                                "\t\t<random_map_order>1</random_map_order>" . CRLF .
                                "\t</filter>", $list);
-
+          $startPos = strpos($list, "<script_name>") + 13;
+          $endPos = strpos($list, "</script_name>");
+          $substr = substr($list, $startPos, $endPos - $startPos);
+          $list = str_ireplace($substr, $aseco->server->gameinfo->scriptname, $list);
           // write out the match settings file
           if (!@file_put_contents($mapsfile, $list)) {
             trigger_error('Could not write match settings file ' . $mapsfile . ' !', E_USER_WARNING);
