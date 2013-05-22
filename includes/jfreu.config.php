5561754d9417addeb3bfd51b0925b7e9951e3ebd
ManiacTwister <maniactwister@s7t.de>
2013-05-23 01:45:26 +0200

    Why literate through a array to create a new array?

--- includes/jfreu.config.php
+++ includes/jfreu.config.php
@@ -13,50 +13,47 @@
    
   global $feature_votes;
   
-	//-> paths to config, vip/vip_team & bans files
-	$conf_file     = 'configs/plugins/jfreu/jfreu.config.xml';
-	$vips_file     = 'configs/plugins/jfreu/jfreu.vips.xml';
-	$bans_file     = 'configs/plugins/jfreu/jfreu.bans.xml';
+  //-> paths to config, vip/vip_team & bans files
+  $conf_file     = 'configs/plugins/jfreu/jfreu.config.xml';
+  $vips_file     = 'configs/plugins/jfreu/jfreu.vips.xml';
+  $bans_file     = 'configs/plugins/jfreu/jfreu.bans.xml';
   $message_file  = 'configs/plugins/jfreu/jfreu.messages.xml'; 
    
-if (file_exists($message_file)) {
-  $aseco->console('Load jfreu message file [' . $message_file . ']');
-	if ($xml = $aseco->xml_parser->parseXml($message_file)) {
+  if (file_exists($message_file)) {
+    $aseco->console('Load jfreu message file [' . $message_file . ']');
+    if ($xml = $aseco->xml_parser->parseXml($message_file)) {
  
-  	//-> player join/leave messages	
-		$player_join  = $xml['JFREU_MESSAGES']['PLAYER_JOIN'][0];
-	  $player_joins = $xml['JFREU_MESSAGES']['PLAYER_JOINS'][0];
-	  $player_left  = $xml['JFREU_MESSAGES']['PLAYER_LEFT'][0];
+    //-> player join/leave messages	
+    $player_join  = $xml['JFREU_MESSAGES']['PLAYER_JOIN'][0];
+    $player_joins = $xml['JFREU_MESSAGES']['PLAYER_JOINS'][0];
+    $player_left  = $xml['JFREU_MESSAGES']['PLAYER_LEFT'][0];
 	
    	//-> prefix for info messages
-	  $message_start = $xml['JFREU_MESSAGES']['MESSAGE_START'][0];   	
+    $message_start = $xml['JFREU_MESSAGES']['MESSAGE_START'][0];   	
 
     $i=1;
- 	  foreach ($xml['JFREU_MESSAGES']['INFO_MESSAGES'][0]['MESSAGE'] as $message) {
- 	     ${'message'.$i} = $message; 
- 	     $i++;
- 	  }
+    foreach ($xml['JFREU_MESSAGES']['INFO_MESSAGES'][0]['MESSAGE'] as $message) {
+      ${'message'.$i} = $message; 
+      $i++;
+    }
 
-  	if ($feature_votes) {
-   	  foreach ($xml['JFREU_MESSAGES']['FEATURED_VOTE_MESSAGES'][0]['MESSAGE'] as $message) {
-   	     ${'message'.$i} = $message; 
- 	       $i++;  
-   	  }
-  	}
-  	if (function_exists('send_window_message')) {
-   	  ${'message'.$i} = $xml['JFREU_MESSAGES']['SEND_WINDOW_MESSAGE'][0];   	
-  	}
+    if ($feature_votes) {
+      foreach ($xml['JFREU_MESSAGES']['FEATURED_VOTE_MESSAGES'][0]['MESSAGE'] as $message) {
+        ${'message'.$i} = $message; 
+        $i++;  
+      }
+    }
+    if (function_exists('send_window_message')) {
+      ${'message'.$i} = $xml['JFREU_MESSAGES']['SEND_WINDOW_MESSAGE'][0];   	
+    }
   
-    $badwordslist = array();
-   	foreach ($xml['JFREU_MESSAGES']['BADWORDS_LIST'][0]['BADWORD'] as $badword) {
-     	  $badwordslist[] = $badword;
-   	}    
+    $badwordslist = $xml['JFREU_MESSAGES']['BADWORDS_LIST'][0]['BADWORD'];
      
                 	
+    } else {
+      trigger_error('Could not read/parse jfreu message file ' . $message_file . ' !', E_USER_WARNING);
+    }
   } else {
-  trigger_error('Could not read/parse jfreu message file ' . $message_file . ' !', E_USER_WARNING);
-  }
-} else {
-  trigger_error('Could not find jfreu message file ' . $message_file . ' !', E_USER_WARNING);
-}   
+    trigger_error('Could not find jfreu message file ' . $message_file . ' !', E_USER_WARNING);
+  }   
 ?>
